4cc54970bd4c83815d4bfc0a43c19cb0163bee29
ayuputrii <ayuarmadani.pm@gmail.com>
2020-09-26 18:15:06 +0700

    Add Controller

--- src/helper/controller.js
+++ src/helper/controller.js
@@ -3,6 +3,6 @@
 const response = require('../../res')
 const connection = require('./connection')
 
-exports = function(req, res) {
-    response.ok('Application REST API Success')
+exports.index = function(req, res) {
+    response.ok('Application REST API Success', res)
 };
