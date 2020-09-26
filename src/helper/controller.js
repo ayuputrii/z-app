b191989ebdcc1985f410838e42f39af79e57f81a
ayuputrii <ayuarmadani.pm@gmail.com>
2020-09-26 17:58:45 +0700

    Add File Controller

---
+++ src/helper/controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const response = require('../../res')
+const connection = require('./connection')
+
+exports = function(req, res) {
+    response.ok('Application REST API Success')
+};
